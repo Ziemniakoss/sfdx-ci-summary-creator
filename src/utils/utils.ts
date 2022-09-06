2df3cc22d7f3818a1448bf202f96b8166acec2e0
Przemek <rawa.przemek@gmail.com>
2022-09-07 00:52:19 +0200

    coveralls report creation

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,5 @@
-import { existsSync, mkdirSync } from "fs";
-import { dirname } from "path";
+import { existsSync, mkdirSync, promises } from "fs";
+import { dirname, join, basename } from "path";
 
 export function mkdirs(path: string) {
   const parentDir = dirname(path);
@@ -19,4 +19,40 @@ export function wrapInArray(a) {
     return a;
   }
   return [a];
+}
+
+export async function getProjectRoot(searchStartPath: string): Promise<string> {
+  if (existsSync(join(searchStartPath, "sfdx-project.json"))) {
+    return searchStartPath;
+  }
+  return getProjectRoot(dirname(searchStartPath));
+}
+
+export async function findFile(
+  fileName: string,
+  directory?: string
+): Promise<string | null> {
+  if (directory == null) {
+    directory = await getProjectRoot(process.cwd());
+  }
+  const dirContent = await promises
+    .readdir(directory)
+    .then((files) => files.map((file) => join(directory, file)));
+  const conntentsWithProps = await Promise.all(
+    dirContent.map((file) => ({ file, properties: promises.lstat(file) }))
+  );
+  const dirs = [];
+  for (const p of conntentsWithProps) {
+    const properties = await p.properties;
+    if (properties.isDirectory()) {
+      dirs.push(p.file);
+    } else if (properties.isFile() && basename(p.file) == fileName) {
+      return p.file;
+    }
+  }
+
+  const foundFilesInSubdirs = await Promise.all(
+    dirs.map((dirName) => findFile(fileName, dirName))
+  );
+  return foundFilesInSubdirs.find((file) => file != null);
 }
