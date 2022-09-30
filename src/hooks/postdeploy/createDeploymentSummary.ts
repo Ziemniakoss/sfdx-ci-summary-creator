faa54d5b24151cf94857b88bd4e3152c3ea78e6a
Ziemniakoss <48589406+Ziemniakoss@users.noreply.github.com>
2022-09-30 08:43:06 +0200

    feature/catch-all
    All errors are handled

--- src/hooks/postdeploy/createDeploymentSummary.ts
+++ src/hooks/postdeploy/createDeploymentSummary.ts
@@ -11,13 +11,32 @@ interface PostDeploymentEvent {
 }
 
 const hook = async function (event: PostDeploymentEvent) {
-  await fs.promises.writeFile("test.json", JSON.stringify(event));
-  const deploymentResult = event.result.response;
-  const promises = [
-    new MarkdownDeploymentSummaryCreator().createSummary(deploymentResult),
-    new JUnitDeploymentSummaryCreator().createSummary(deploymentResult),
-    new CoverallsCoverageReportCreator().createSummary(deploymentResult),
+  const deploymentResult = event?.result?.response;
+  if (deploymentResult == null) {
+    return;
+  }
+  const promises: Promise<any>[] = [
+    new MarkdownDeploymentSummaryCreator()
+      .createSummary(deploymentResult)
+      .catch(printError),
+    new JUnitDeploymentSummaryCreator()
+      .createSummary(deploymentResult)
+      .catch(printError),
+    new CoverallsCoverageReportCreator()
+      .createSummary(deploymentResult)
+      .catch(printError),
   ];
-  return Promise.all(promises);
+  return Promise.all(promises).catch(printError);
 };
+
+function printError(error) {
+  console.log("AAAAAAAAAA");
+  console.error(
+    "============================Error in hook======================================="
+  );
+  console.error(error);
+  console.error(
+    "====================================For========================================="
+  );
+}
 export default hook;
