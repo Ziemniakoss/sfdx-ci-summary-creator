d85c751a513ffa548e1221035cf74720c75e4af8
Przemek <rawa.przemek@gmail.com>
2023-03-20 21:17:49 +0100

    add timer for all report generation

--- src/hooks/predeploy/startIndexing.ts
+++ src/hooks/predeploy/startIndexing.ts
@@ -1,17 +1,17 @@
-import {startIndexing} from "../../utils/metadataIndex";
+import { startIndexing } from "../../utils/metadataIndex";
 import Environment from "../../utils/Environment";
-import {ENV_VARS_NAMES} from "../../utils/constants";
+import { ENV_VARS_NAMES } from "../../utils/constants";
 
-const TIMER_NAME = "metadata indexing"
+const TIMER_NAME = "CIRC: metadata indexing time";
 
 export async function hook() {
-    const env = new Environment()
-    const areTimersEnabled = env.getBooleanVar(ENV_VARS_NAMES.COMMON.ENABLE_TIMERS)
-    if(areTimersEnabled) {
-        console.time(TIMER_NAME)
+    const env = new Environment();
+    const areTimersEnabled = env.getBooleanVar(ENV_VARS_NAMES.COMMON.ENABLE_TIMERS);
+    if (areTimersEnabled) {
+        console.time(TIMER_NAME);
     }
-    const indexingPromise = startIndexing()
-    if(areTimersEnabled) {
-        indexingPromise.then(() => console.timeEnd(TIMER_NAME))
+    const indexingPromise = startIndexing();
+    if (areTimersEnabled) {
+        indexingPromise.then(() => console.timeEnd(TIMER_NAME));
     }
 }
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,9 @@
-import {existsSync, mkdirSync, promises} from "fs";
-import {dirname, join,} from "path";
+import { existsSync, mkdirSync, promises } from "fs";
+import { dirname, join, relative } from "path";
+import { getPathFromIndex } from "./metadataIndex";
+import { FILE_EXTENSIONS } from "./constants";
+
+const PROJECT_DEFINITION_FILE_NAME = "sfdx-project.json";
 
 export function mkdirs(path: string) {
     const parentDir = dirname(path);
@@ -31,7 +35,7 @@ export async function getProjectRoot(searchStartPath: string): Promise<string> {
 export async function getProjectDirectories(): Promise<string[]> {
     const rootProjectDirectory = await getProjectRoot(process.cwd());
     return promises
-        .readFile(join(rootProjectDirectory, "sfdx-project.json"), "utf-8")
+        .readFile(join(rootProjectDirectory, PROJECT_DEFINITION_FILE_NAME), "utf-8")
         .then(JSON.parse)
         .then((projectDefinition) => {
             return projectDefinition.packageDirectories.map((projectDir) =>
@@ -40,7 +44,32 @@ export async function getProjectDirectories(): Promise<string[]> {
         });
 }
 
-export async function formatStackTrace(stackTrace:string) {
-    co
+export async function formatStackTrace(stackTrace: string) {
+    if (stackTrace == null) {
+        return "";
+    }
+    const stackEntries = stackTrace.replace("\n\r", "\n").split("\n");
+    const stack = await Promise.all(stackEntries.map((entry) => formatStackEntry(entry))).then((formattedEntries) =>
+        formattedEntries.join("\n")
+    );
+    return "\n" + stack;
+}
 
+const INDENT = "        ";
+
+async function formatStackEntry(stackEntry: string): Promise<string> {
+    stackEntry = stackEntry.trim();
+    if (!stackEntry.startsWith("Class.")) {
+        return `${INDENT}${stackEntry}`;
+    }
+
+    const [fullEntityName, _empty1, _line, lineNumber] = stackEntry.replace(",", " ").replace(":", " ").split(" ");
+    const [_entityType, entityName, method] = fullEntityName.split(".");
+    const nameInIndex = `${entityName}.${FILE_EXTENSIONS.APEX_CLASS}`;
+    const path = await getPathFromIndex(nameInIndex);
+    if (path != null) {
+        const relativePath = relative(process.cwd(), path);
+        return `${INDENT}${relativePath}:${lineNumber} in method ${method}`;
+    }
+    return `${INDENT}${stackEntry}`;
 }
